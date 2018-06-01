6c1169314f46e350c267b1f4d5677cd98a24efa1
chentianyu1 <chentianyu1@100tal.com>
2018-06-01 18:23:15 +0800

    add pre-req script

--- JavaScript/createCoursePreRequestScript.js
+++ JavaScript/createCoursePreRequestScript.js
@@ -29,70 +29,39 @@ function addDate(dateString, days) {
 
 
 // 获取第一个目录日期
+var beginCatalogId = pm.environment.get("BEGIN_CATALOG_ID");
+var catalogCount = pm.environment.get("CATALOG_COUNT");
 var firstDateString = pm.environment.get("DAY");
 var teacherId = pm.environment.get("SELECT_TEACHER_ID");
 var start_hour = pm.environment.get("START_HOUR");
 var start_minute = pm.environment.get("START_MINUTE");
 var end_hour = pm.environment.get("END_HOUR");
 var end_minute = pm.environment.get("END_MINUTE");
-
-
-// 设置JSON
-// 定义JSON格式变量
-var planinfosObject = [
-    {
-        "catalogId":"77753",
-        "mode":"1",
-        "pattern":"1",
-        "planName":"课时1",
-        "teacherId":"teacherId",
-        "day":addDate(firstDateString, 1),
-        "hourStart":"start_hour",
-        "muniteStart":"start_minute",
-        "hourEnd":"end_hour",
-        "mumiteEnd":"end_minute"
-    }
-];
-// JSON格式化，直接用变量不行
-// js通过 JSON.stringify()格式化字符串；通过JSON.parse()解析JSON对象
-planinfosObject = JSON.stringify(planinfosObject);
-console.log("###########planinfosObject:" + planinfosObject);
-
-// 应该用个for循环来创建
-var catologCount = pm.environment.get("CATOLOG_COUNT");
-// 1、定义一个JOSN空数组
-var catologs = new Array();
-// 2、通过for循环增加object
-for (int i; i<catologCount; i++) {
-    var object = [
-        {
-            "catalogId":"77753",
-            "mode":"1",
-            "pattern":"1",
-            "planName":"课时1",
-            "teacherId":"teacherId",
-            "day":addDate(firstDateString, 1),
-            "hourStart":"start_hour",
-            "muniteStart":"start_minute",
-            "hourEnd":"end_hour",
-            "mumiteEnd":"end_minute"
-        }
-    ];
-    catologs[i] = object;
-}
-
-
-
-// 清理环境变量
-pm.environment.unset("planinfos_object");
-
-// 设置环境变量"planinfos_object"
-pm.environment.set("planinfos_object", planinfosObject);    
+var planinfosObject = [];
 
 
 // 创建课程：一个目录
 function createCourseWithOneCatolog() {
-    
+    // 设置JSON
+    // 定义JSON格式变量
+    var object = [
+        {
+            "catalogId":beginCatalogId,
+            "mode":"1",
+            "pattern":"1",
+            "planName":"课时1",
+            "teacherId":teacherId,
+            "day":addDate(firstDateString, 1),
+            "hourStart":start_hour,
+            "muniteStart":start_minute,
+            "hourEnd":end_hour,
+            "mumiteEnd":end_minute
+        }
+    ];
+    // JSON格式化，直接用变量不行
+    // js通过 JSON.stringify()格式化字符串；通过JSON.parse()解析JSON对象
+    planinfosObject = JSON.stringify(object);
+    console.log("###########planinfosObject:" + planinfosObject);
 }
 
 // 创建课程：多个目录
@@ -101,10 +70,46 @@ function createCourseWithMultipleCatolog() {
 }
 
 // 根据数量创建（）
-function createCourseWithCatologs(numbers) {
-    
+function createCourseWithCatalogs(firstId, count) {
+    // 应该用个for循环来创建
+    // 1、定义一个JOSN空数组
+    //var catalogs = new Array();
+    var catalogs = [];
+    // 2、通过for循环增加object
+    for (var i=0; i<count; i++) {
+        var catalogId = Number(firstId) + i;
+        var object = {
+                "catalogId":catalogId,
+                "mode":"1",
+                "pattern":"1",
+                "planName":"课时1",
+                "teacherId":teacherId,
+                "day":addDate(firstDateString, i),
+                "hourStart":start_hour,
+                "muniteStart":start_minute,
+                "hourEnd":end_hour,
+                "mumiteEnd":end_minute
+            };
+        catalogs[i] = object;
+    }
+    console.log("catalogs: " + catalogs);
+
+    // JSON格式化
+    planinfosObject = JSON.stringify(catalogs);
+    console.log("###########planinfosObject:" + planinfosObject);
 }
 
 
+
+// 清理环境变量
+pm.environment.unset("planinfos_object");
+
+// 创建logic
+createCourseWithCatalogs(beginCatalogId, catalogCount);
+//createCourseWithOneCatolog();
+
+
+// 设置环境变量"planinfos_object"
+pm.environment.set("planinfos_object", planinfosObject);
 
 
